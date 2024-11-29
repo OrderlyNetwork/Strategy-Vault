5c680acca8ec753c7c8b08d43ce4f7818d8ba585
zibin xu <zibin@orderly.network>
2024-11-29 09:41:18 +0000

    Feature/vault creation

--- contracts/utils/ContractFactory.sol
+++ contracts/utils/ContractFactory.sol
@@ -2,36 +2,52 @@
 pragma solidity ^0.8.24;
 
 import {CREATE3} from "solady/src/utils/CREATE3.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 /// @title Factory for deploying contracts to deterministic addresses via CREATE3
-/// @author zefram.eth, SKYBIT
 /// @notice Enables deploying contracts using CREATE3. Each deployer (msg.sender) has
 /// its own namespace for deployed addresses.
-contract ContractFactory {
-    function deploy(bytes32 salt, bytes memory creationCode) external payable returns (address) {
+
+contract ContractFactory is Ownable2Step {
+    mapping(address => bool) public isManager;
+
+    error NoAccess();
+
+    event ContractDeployed(address deployedContract);
+
+    constructor(address owner) Ownable(owner) {
+        _transferOwnership(owner);
+    }
+
+    modifier onlyManager() {
+        if (msg.sender != owner() && !isManager[msg.sender]) {
+            revert NoAccess();
+        }
+        _;
+    }
+
+    function deploy(bytes32 salt, bytes memory creationCode) external onlyManager returns (address) {
         // hash salt with the deployer address to give each deployer its own namespace
         salt = keccak256(abi.encodePacked(msg.sender, salt));
-        return CREATE3.deployDeterministic(creationCode, salt);
+        address contractAddress = CREATE3.deployDeterministic(creationCode, salt);
+
+        emit ContractDeployed(contractAddress);
+        return contractAddress;
     }
 
     function getDeployed(bytes32 salt) external view returns (address) {
-        // hash salt with the deployer address to give each deployer its own namespace
+        // hash salt with keythe deployer address to give each deployer its own namespace
         salt = keccak256(abi.encodePacked(msg.sender, salt));
         return CREATE3.predictDeterministicAddress(salt);
     }
 
-    function deployByCreate2(bytes32 salt, bytes memory creationCode) external returns (address) {
-        salt = keccak256(abi.encodePacked(msg.sender, salt));
-
-        address deployedContract;
-
-        assembly {
-            deployedContract := create2(0, add(creationCode, 0x20), mload(creationCode), salt)
-            if iszero(deployedContract) {
-                returndatacopy(0, 0, returndatasize())
-                revert(0, returndatasize())
+    function setManagers(address[] calldata managers, bool knob) external onlyOwner {
+        for (uint256 i = 0; i < managers.length; i++) {
+            // Already set the same value
+            if (isManager[managers[i]] == knob) {
+                continue;
             }
+            isManager[managers[i]] = knob;
         }
-        return deployedContract;
     }
 }
---
+++ scripts/deploy/deploy_protocol_vault_by_factory.js
@@ -0,0 +1,39 @@
+const { ethers } = require("hardhat")
+
+async function main() {
+    const ProtocolVault = await ethers.getContractFactory("ProtocolVault");
+    const protocol_vault_bytecode = (await ethers.getContractFactory("ProtocolVault")).bytecode;
+    const proxy_bytecode = (await ethers.getContractFactory("ERC1967Proxy")).bytecode;
+    
+    
+    const initializeData = ProtocolVault.interface.encodeFunctionData(
+        "initialize",
+        []
+    );
+    
+    const implAddr = "";
+    const constructorArgs = ethers.AbiCoder.defaultAbiCoder().encode(
+        ["address", "bytes"],
+        [implAddr, initializeData]
+    );
+    console.log(constructorArgs);
+
+    //final bytecode
+    const bytecode = ethers.concat([
+        proxy_bytecode,
+        constructorArgs
+    ]);
+
+    //Deploy contract by factory
+    const salt = ethers.ZeroHash;
+    factoryAddr = "0x52f1795B4B0c15877805c8C90cA7D6E2567aB805"
+    const contractFactory = await ethers.getContractAt(
+        "ContractFactory",
+        addfactoryAddrress
+    )
+    const tx = await contractFactory.deploy(salt,bytecode)
+    await tx.wait()
+    console.log("Contract deployed Done")
+}
+
+main()
