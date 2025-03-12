fc5385e9bd5782d781d65d832379360c87eecfe9
zibin xu <zibin@orderly.network>
2025-03-12 18:35:28 +0800

    fix scripts

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -108,7 +108,6 @@ async function deployProtocolVault(env) {
 
     const implAddr = await deployProtocolVaultImpl(ProtocolVault);
     //const implAddr = "0x83F367998EC5C78C107F32666B053D6A8991D773";
-    console.log("ProtocolVault Impl deployed to:", implAddr);
     
     const [owner] = await ethers.getSigners();
 
