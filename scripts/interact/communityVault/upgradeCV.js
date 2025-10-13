4bb36e4cc6cac6cf973d8d2557771b3fd8d8c2d9
zibin xu <zibin@orderly.network>
2025-10-13 22:26:46 +0800

    add scripts

---
+++ scripts/interact/communityVault/upgradeCV.js
@@ -0,0 +1,40 @@
+const { ethers } = require("hardhat")
+const hre = require("hardhat")
+const deployment = require('../../../deployment/deployment.json');
+const cvDeployment = require('../../../deployment/community.json');
+const { verifyContract } = require("../../utils/verifyContract");
+
+async function main() {
+    const ProtocolVault = await ethers.getContractFactory("ProtocolVault");
+    const ProtocolVaultContract = await ProtocolVault.deploy();
+    await ProtocolVaultContract.waitForDeployment();
+
+    const implAddr = ProtocolVaultContract.target;
+    console.log("ProtocolVaultContract Impl deployed to:", implAddr);
+
+    // verify
+    await verifyContract(implAddr, [], "ProtocolVault Implementation");
+
+    //upgrade
+    //!need to change with your env
+    const cv = "woo"
+
+    const protocolVault = await ethers.getContractAt(
+        "ProtocolVault",
+        cvDeployment[cv].address
+    )
+
+    const impl = implAddr;
+    tx = await protocolVault.upgradeToAndCall(impl, "0x")
+    await tx.wait();
+    console.log("upgrade protocol vault contract successfully");
+
+}
+
+
+
+main().catch(error => {
+    console.error(error)
+    process.exitCode = 1
+})
+
