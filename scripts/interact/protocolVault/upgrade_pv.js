7b1014b398405700d6dd3443edb89cec5f0e8037
zibin xu <zibin@orderly.network>
2025-10-14 10:36:52 +0800

    add config

--- scripts/interact/protocolVault/upgrade_pv.js
+++ scripts/interact/protocolVault/upgrade_pv.js
@@ -1,6 +1,6 @@
 const { ethers } = require("hardhat")
 const hre = require("hardhat")
-const deployment = require('../../../deployment.json');
+const deployment = require('../../../deployment/deployment.json');
 const { verifyContract } = require("../../utils/verifyContract");
 
 async function main() {
