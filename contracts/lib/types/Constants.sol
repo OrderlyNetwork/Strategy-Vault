a3726ae78a77417fd945a82517bd06b7579b002b
zibin xu <zibin@orderly.network>
2025-11-18 12:24:07 +0800

    ed25519 stg

--- contracts/lib/types/Constants.sol
+++ contracts/lib/types/Constants.sol
@@ -15,7 +15,7 @@ bytes32 constant TYPE_HASH = 0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa
 /// @dev `keccak256("DexRequest(uint8 payloadType,uint256 nonce,address receiver,uint256 amount,bytes32 vaultId,string token,string dexBrokerId)")`.
 bytes32 constant REQUEST_HASH = 0x590ef38f093814e411b876bc59d8020504481133ef17b2b49abbdedc31d57084;
 
-address constant ED25519 = 0x1DEb0Ee80d89801B948e98CF9E22A985CBF33a48;
+address constant ED25519 = 0x9A7Fa019B2848E075768966caFe42b203B23b4F1;
 
 
 /*
